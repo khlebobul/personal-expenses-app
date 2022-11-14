55d46cb5fade70fcec99f79077c8930e37495ae4
Gleb Shalimov <sbgleb10@gmail.com>
2022-11-14 14:31:04 +0300

    colombining widgets

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,19 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
       ),
     );
   }
