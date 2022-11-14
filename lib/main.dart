85e5c0687e6be65247a3bec225d569ae7514fe8e
Gleb Shalimov <sbgleb10@gmail.com>
2022-11-14 16:01:46 +0300

    install date package and formating date

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expences_app/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -54,7 +55,7 @@ class MyHomePage extends StatelessWidget {
                       )),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             color: Colors.purple,
@@ -70,7 +71,7 @@ class MyHomePage extends StatelessWidget {
                               fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat('yMMMd').format(tx.date),
                           style: TextStyle(color: Colors.grey),
                         )
                       ],
